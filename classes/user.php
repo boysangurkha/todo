be8414b5818594eadb1a5f8c84010517ce8b4160
boysangurkha <r0797383@student.thomasmore.be>
2022-08-10 16:28:23 +0200

    User kan registreren

---
+++ classes/user.php
@@ -0,0 +1,93 @@
+<?php
+    include_once(__DIR__ . "/db.php");
+
+    class User {
+        private $email;
+        private $username;
+        private $password;
+
+        public function getEmail()
+        {
+                return $this->email;
+        }
+
+        public function setEmail($email)
+        {
+            if(empty($email)){
+                throw new Exception("Email cannot be empty.");
+            }
+            $this->email = $email;
+            return $this;
+        }
+
+        public function getUsername()
+        {
+                return $this->username;
+        }
+
+        //set username but checks if not duplicate in database
+        public function setUsername($username)
+        {
+            $conn = Db::getInstance();
+            $stmt = $conn->prepare("SELECT username FROM users WHERE username = :username");
+            $stmt -> bindValue(":username", $username);
+            $stmt -> execute();
+            // $user = ($stmt->fetch());
+
+            //     if ($user) {
+            //             throw new Exception("Username already exists");
+            //             return false;
+            //     }
+                $this->username = $username;
+                return $this->username;
+        }
+
+        public function getPassword()
+        {
+            return $this->password;
+        }
+
+        public function setPassword( $password )
+        {
+            if(strlen($password) < 5){
+                throw new Exception("Passwords must be longer than 6 characters.");
+            }
+
+            $this->password = $password;
+            return $this;
+        }
+
+        public function getError()
+        {
+                return $this->error;
+        }
+        public function setError($error)
+        {
+                $this->error = $error;
+
+                return $this;
+        }
+
+        public function register()
+        {
+                $conn = Db::getInstance();
+                $statement = $conn->prepare("select * from users where email = :email;");
+                $statement->bindValue(':email', $this->email);
+                $statement->execute();
+                $user = ($statement->fetch());
+                if ($user) {
+                        throw new Exception("Username already taken");
+                        return false;
+                }
+                $options = [
+                    'cost' => 12
+                ];
+                $hash = password_hash($this->password, PASSWORD_DEFAULT, $options);
+                $statement = $conn->prepare("insert into users (username, email, password) values (:username, :email, :password)");
+                $statement->bindValue(":username", $this->username);
+                $statement->bindValue(":email", $this->email);
+                $statement->bindValue(":password", $hash);
+                $statement->execute();
+                return true;
+        }
+    }
---
+++ register.php
@@ -0,0 +1,41 @@
+<?php 
+
+    if(!empty($_POST)){
+        try {
+            include_once(__DIR__ . "/classes/User.php");
+
+            $user = new User();
+            $user->setUsername($_POST['username']);
+            $user->setEmail($_POST['email']);
+            $user->setPassword(($_POST['password']));
+            $user->register();
+
+            session_start();
+            $_SESSION['email'] = $user->getEmail();            
+            header("Location: index.php");
+        }
+        catch(Throwable $error) {
+            $error = $error->getMessage();
+        }
+    }
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register - ToDo</title>
+</head>
+<body>
+    <form method="post" action=>
+        <h1>Register</h1>
+        <input name="email" type="email" placeholder="Email">
+        <input name="username" type="username" placeholder="Username">
+        <input name="password" type="password" placeholder="Password">
+        <button class="btn" name="register" type="submit" value="Register">
+            <span id="registerBtn">SIGN UP</span>
+        </button>
+    </form>
+</body>
+</html>
